aaf4a97e755bcadbb8f7dfae316c064220c7d6db
Pascal Fromentin <pndfromentin@gmail.com>
2020-01-23 11:49:56 +0100

    Création du formulaire d'ajout d'une annonce

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -7,6 +7,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Ad;
 use App\Repository\AdRepository;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Form\AdType;
 
 
 class AdController extends AbstractController {
@@ -26,6 +28,45 @@ class AdController extends AbstractController {
         ]);
     }
 
+    /**
+     * Permet de créer une annonce
+     *
+     * @Route("/ads/new", name="ads_create")
+     *
+     * @return Response
+     */
+
+        public function create() {
+
+            $ad = new Ad();
+
+            /*
+            // $form est un très gros Objet qui contitnet plein de méthodes
+
+            $form = $this->createFormBuilder($ad)
+                        ->add('title')
+                        ->add('introduction')
+                        ->add('content')
+                        ->add('rooms')
+                        ->add('price')
+                        ->add('coverImage')
+                        ->add('save', SubmitType::class, [
+                            'label' => 'Créer la nouvelle annonce',
+                            'attr' => [
+                                'class' => 'btn btn-primary']
+                        ])
+                        ->getForm();
+
+            */
+
+            /* On a créer à la place de tout ce code trop volumineux pour un controller, la classe form, qui va nous permettre d'instancier un form externe avec la fonction createForm qui permet de créer un formulaire externe */
+
+            $form = $this->createForm(AdType::class, $ad);
+
+            return $this->render('ad/new.html.twig', [
+                'form' => $form->createView()
+            ]);
+        }
 
     /**
      * Permet d'afficher une seule annonce
@@ -57,15 +98,4 @@ class AdController extends AbstractController {
             ]);
     }
 
-    /**
-     * Permet de créer une annonce
-     *
-     * @Route("/ads/new", name="ads_create")
-     *
-     * @return Response
-     */
-
-        public function create() {
-
-        }
 }
